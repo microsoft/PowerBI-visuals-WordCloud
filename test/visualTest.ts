0dcd2634a782caecfdbac55238b7e973149853b6
mvgaliev <maxgaliev@mail.ru>
2017-11-01 12:35:47 +0300

    added UT for capabilities (#27)

--- test/visualTest.ts
+++ test/visualTest.ts
@@ -528,5 +528,29 @@ module powerbi.extensibility.visual.test {
                 expect(item.index).toBe(5);
             });
         });
+
+        describe("Capabilities tests", () => {
+            it("all items having displayName should have displayNameKey property", () => {
+                jasmine.getJSONFixtures().fixturesPath = "base";
+
+                let jsonData = getJSONFixture("capabilities.json");
+
+                let objectsChecker: Function = (obj) => {
+                    for (let property in obj) {
+                        let value: any = obj[property];
+
+                        if (value.displayName) {
+                            expect(value.displayNameKey).toBeDefined();
+                        }
+
+                        if (typeof value === "object") {
+                            objectsChecker(value);
+                        }
+                    }
+                };
+
+                objectsChecker(jsonData);
+            });
+        });
     });
 }
