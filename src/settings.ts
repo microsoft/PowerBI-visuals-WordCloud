d8d25657ee4cdc83476702e95a4dbda5c86324da
Aleksandr Levochkin <v-allevockin@microsoft.com>
2023-04-27 09:50:38 +0200

    Created formatting model

--- src/settings.ts
+++ src/settings.ts
@@ -24,54 +24,244 @@
  *  THE SOFTWARE.
  */
 
-// powerbi.extensibility.utils.dataview
-import { dataViewObjectsParser } from "powerbi-visuals-utils-dataviewutils";
-import DataViewObjectsParser = dataViewObjectsParser.DataViewObjectsParser;
+import { formattingSettings } from "powerbi-visuals-utils-formattingmodel";
 
-export class WordCloudSettings extends DataViewObjectsParser {
+import FormattingSettingsCard = formattingSettings.Card;
+import FormattingSettingsModel = formattingSettings.Model;
+import FormattingSettingsSlice = formattingSettings.Slice;
+import { WordCloudDataPoint } from "./dataInterfaces";
+import { ColorHelper } from "powerbi-visuals-utils-colorutils";
+import uniqBy from "lodash.uniqby";
+
+export class WordCloudSettings extends FormattingSettingsModel {
     public general: GeneralSettings = new GeneralSettings();
     public dataPoint: DataPointSettings = new DataPointSettings();
     public stopWords: StopWordsSettings = new StopWordsSettings();
     public rotateText: RotateTextSettings = new RotateTextSettings();
     public performance: PerformanceSettings = new PerformanceSettings();
+
+    public cards: FormattingSettingsCard[] = [this.general, this.dataPoint, this.stopWords, this.rotateText, this.performance];
+
+    public initColors(dataPoints: WordCloudDataPoint[]) {
+        const wordCategoriesIndex: number[] = [];
+        const colors: FormattingSettingsSlice[] = [];
+
+        const uniqueDataPoints: WordCloudDataPoint[] = uniqBy(dataPoints, (dataPoint: WordCloudDataPoint) => dataPoint.wordIndex);
+        this.dataPoint.slices = [this.dataPoint.slices[0]];
+
+        uniqueDataPoints.forEach((dataPoint: WordCloudDataPoint) => {
+            if (wordCategoriesIndex.indexOf(dataPoint.wordIndex) === -1) {
+                wordCategoriesIndex.push(dataPoint.wordIndex);
+                colors.push(new formattingSettings.ColorPicker({
+                    name: dataPoint.text,
+                    displayName: dataPoint.text,
+                    selector: ColorHelper.normalizeSelector(
+                        dataPoint.selectionIds[0].getSelector(),
+                        false),
+                    
+                    value: { value: dataPoint.color }
+                }));
+            }
+        });
+
+        colors.forEach(color => this.dataPoint.slices.push(color));
+        //this.dataPoint.slices = colors;
+    }
 }
 
-export class GeneralSettings {
+ export class GeneralSettings extends FormattingSettingsCard {
     public static FontSizePercentageFactor: number = 1;
     public static MinFontSize: number = 1;
 
-    public minRepetitionsToDisplay: number = 1;
-    public maxNumberOfWords: number = 200;
-    public minFontSize: number = 20 / GeneralSettings.FontSizePercentageFactor;
-    public maxFontSize: number = 100 / GeneralSettings.FontSizePercentageFactor;
-    public isBrokenText: boolean = true;
-    public isPunctuationsCharacters: boolean = false;
+    public name = "general";
+    public displayNameKey = "Visual_General";
+
+    public minRepetitionsToDisplay = new formattingSettings.NumUpDown({
+        name: "minRepetitionsToDisplay",
+        displayNameKey: "Visual_WordCloud_minRepetitionsToDisplay",
+        value: 1,
+    });
+
+    public maxNumberOfWords = new formattingSettings.NumUpDown({
+        name: "maxNumberOfWords",
+        displayNameKey: "Visual_WordCloud_MaxNumberWords",
+        value: 200,
+    });
+
+    public minFontSize = new formattingSettings.NumUpDown({
+        name: "minFontSize",
+        displayNameKey: "Visual_MinFontSize",
+        value: 20 / GeneralSettings.FontSizePercentageFactor,
+        options: {
+            minValue: {
+                type: powerbi.visuals.ValidatorType.Min,
+                value: 1,
+            },
+            maxValue: {
+                type: powerbi.visuals.ValidatorType.Max,
+                value: 100,
+            },
+        },
+    });
+
+    public maxFontSize = new formattingSettings.NumUpDown({
+        name: "maxFontSize",
+        displayNameKey: "Visual_MaxFontSize",
+        value: 100 / GeneralSettings.FontSizePercentageFactor,
+        options: {
+            minValue: {
+                type: powerbi.visuals.ValidatorType.Min,
+                value: 1,
+            },
+            maxValue: {
+                type: powerbi.visuals.ValidatorType.Max,
+                value: 100,
+            },
+        },
+    });
+
+    public isBrokenText = new formattingSettings.ToggleSwitch({
+        name: "isBrokenText",
+        displayNameKey: "Visual_WordCloud_WordBreaking",
+        value: true,
+    });
+
+    public isPunctuationsCharacters = new formattingSettings.ToggleSwitch({
+        name: "isPunctuationsCharacters",
+        displayNameKey: "Visual_WordCloud_SpecialCharacters",
+        value: false,
+    });
+    
+    public slices: FormattingSettingsSlice[] = [this.minRepetitionsToDisplay, this.maxNumberOfWords, this.minFontSize, this.maxFontSize, this.isBrokenText, this.isPunctuationsCharacters]; 
 }
 
-export class DataPointSettings {
-    defaultColor: string = null;
+export class DataPointSettings extends FormattingSettingsCard {
+    public name = "dataPoint";
+    public displayNameKey = "Visual_DataColors";
+
+    public defaultColor = new formattingSettings.ColorPicker({
+        name: "defaultColor",
+        displayNameKey: "Visual_DefaultColor",
+        value: {
+            value: null
+        },
+    });
+
+    public slices: FormattingSettingsSlice[] = [this.defaultColor];
 }
 
-export class StopWordsSettings {
-    public show: boolean = true;
-    public isDefaultStopWords: boolean = false;
-    public words: string = null;
+export class StopWordsSettings extends FormattingSettingsCard {
+    public name = "stopWords";
+    public displayNameKey = "Visual_WordCloud_StopWords";
+
+    public show = new formattingSettings.ToggleSwitch({
+        name: "show",
+        displayNameKey: "Visual_Show",
+        value: true,
+        topLevelToggle: true,
+    });
+
+    public isDefaultStopWords = new formattingSettings.ToggleSwitch({
+        name: "isDefaultStopWords",
+        displayNameKey: "Visual_WordCloud_DefaultStopWords",
+        value: false,
+    });
+
+    public words = new formattingSettings.TextInput({
+        name: "words",
+        displayNameKey: "Visual_WordCloud_Words",
+        value: "",
+        placeholder: "",
+    });
+
+    public slices: FormattingSettingsSlice[] = [this.show, this.isDefaultStopWords, this.words];
 }
 
-export class RotateTextSettings {
+export class RotateTextSettings extends FormattingSettingsCard {
     public static MinAngle: number = -180;
     public static MaxAngle: number = 180;
 
     public static MinNumberOfWords: number = 1;
     public static MaxNumberOfWords: number = 2500;
 
-    public show: boolean = true;
-    public minAngle: number = -60;
-    public maxAngle: number = 90;
-    public maxNumberOfOrientations: number = 2;
+    public name = "rotateText";
+    public displayNameKey = "Visual_RotateText";
+
+    public show = new formattingSettings.ToggleSwitch({
+        name: "show",
+        displayNameKey: "Visual_Show",
+        value: true,
+        topLevelToggle: true,
+    });
+
+    public minAngle = new formattingSettings.NumUpDown({
+        name: "minAngle",
+        displayNameKey: "Visual_MinAngle",
+        value: -60,
+        options: {
+            minValue: {
+                type: powerbi.visuals.ValidatorType.Min,
+                value: RotateTextSettings.MinAngle,
+            },
+            maxValue: {
+                type: powerbi.visuals.ValidatorType.Max,
+                value: RotateTextSettings.MaxAngle,
+            },
+        },
+    });
+
+    public maxAngle = new formattingSettings.NumUpDown({
+        name: "maxAngle",
+        displayNameKey: "Visual_MaxAngle",
+        value: 90,
+        options: {
+            minValue: {
+                type: powerbi.visuals.ValidatorType.Min,
+                value: RotateTextSettings.MinAngle,
+            },
+            maxValue: {
+                type: powerbi.visuals.ValidatorType.Max,
+                value: RotateTextSettings.MaxAngle,
+            },
+        },
+    });
+
+    public maxNumberOfOrientations = new formattingSettings.NumUpDown({
+        name: "maxNumberOfOrientations",
+        displayNameKey: "Visual_WordCloud_MaxOrientationNumber",
+        value: 2,
+    });
+
+    public slices: FormattingSettingsSlice[] = [this.show, this.minAngle, this.maxAngle, this.maxNumberOfOrientations];
 }
 
-export class PerformanceSettings {
-    public preestimate: boolean = false;
-    public quality: number = 40;
+export class PerformanceSettings extends FormattingSettingsCard {
+    public name = "performance";
+    public displayNameKey = "Visual_Performance";
+
+    public preestimate = new formattingSettings.ToggleSwitch({
+        name: "preestimate",
+        displayNameKey: "Visual_PreestimateWordCount",
+        value: false,
+    });
+
+    public quality = new formattingSettings.NumUpDown({
+        name: "quality",
+        displayNameKey: "Visual_Quality",
+        value: 40,
+        options: {
+            minValue: {
+                type: powerbi.visuals.ValidatorType.Min,
+                value: 1,
+            },
+            maxValue: {
+                type: powerbi.visuals.ValidatorType.Max,
+                value: 100,
+            },
+            unitSymbolAfterInput: true,
+            unitSymbol: "%"
+        },
+    });
+
+    public slices: FormattingSettingsSlice[] = [this.preestimate, this.quality];
 }
